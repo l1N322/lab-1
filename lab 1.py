c22d81bc19bf0cf721d838dcfa41f1a25c42ee23
Danil <pro100.dandi@yandex.ru>
2025-09-16 22:12:11 +0300

    Седьмые изменения

--- lab 1.py
+++ lab 1.py
@@ -4,3 +4,4 @@ for x in range(0, 2):
         for z in range(0, 2):
             for w in range(0, 2):
                 if (((x <= z)and(z <= w))or(y == (x or z)))==0:
+                    print(y, z, w, x)
